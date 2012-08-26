ae28dc9bd80202ad51a6026aeabdf1cf873de508
templeman <samuel.templeman@me.com>
2012-08-26 01:07:25 -0700

    Pull Google Analytics tracking code into <head>

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -15,7 +15,19 @@ $this->load->helper('text');
 		echo $this->sprinkle->output();
 		echo $this->sprinkle->output('browser-fixes');
 	?>
+	<script type="text/javascript">
 
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-19370004-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
 </head>
 
 <body id="<?php echo $page_id; ?>" <?php if(!empty($body_extras)) { echo $body_extras; } ?>>
