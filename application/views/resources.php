d0d65ff1314eaed7a39b3acc9f7fdba6f49c43f8
templeman <samuel.templeman@me.com>
2013-01-29 22:37:26 -0800

    Restructure resources page links, clean up css

--- application/views/resources.php
+++ application/views/resources.php
@@ -1,15 +1,33 @@
 <h2>other resources</h2>
 <ul class="panels">
 	<li>
-	<a href="http://www.oregon.gov/OBPE/"><span></span></a><img src="/image/obpe.gif" alt="obpe logo" />Oregon Board of Psychologist Examiners</li>
+    <a href="http://www.oregon.gov/OBPE/">
+    <img src="/image/obpe.gif" alt="obpe logo" />Oregon Board of Psychologist Examiners
+    </a>
+  </li>
 	<li>
-	<a href="http://www.oregon.gov/BLSW/"><span></span></a><img src="/image/blcsw.jpg" alt="board logo" />Oregon Board of Clinical Social Workers</li>
+    <a href="http://www.oregon.gov/BLSW/">
+    <img src="/image/blcsw.jpg" alt="board logo" />Oregon Board of Clinical Social Workers
+    </a>
+  </li>
 	<li>
-	<a href="http://www.osbn.state.or.us"><span></span></a><img src="/image/osbn.jpg" alt="osbn logo" />Oregon State Board of Nursing</li>
+    <a href="http://www.osbn.state.or.us">
+    <img src="/image/osbn.jpg" alt="osbn logo" />Oregon State Board of Nursing
+    </a>
+  </li>
 	<li>
-	<a href="http://opa.org"><span></span></a><img src="/image/opa.gif" alt="opa logo" />Oregon Psychological Association</li>
+    <a href="http://opa.org">
+    <img src="/image/opa.gif" alt="opa logo" />Oregon Psychological Association
+    </a>
+  </li>
 	<li>
-	<a href="http://www.apa.org"><span></span></a><img src="/image/apa.gif" alt="apa logo" />American Psychological Association</li>
+    <a href="http://www.apa.org">
+    <img src="/image/apa.gif" alt="apa logo" />American Psychological Association
+    </a>
+  </li>
 	<li>
-	<a href="http://www.psychologicalscience.org"><span></span></a><img src="/image/aps.gif" alt="aps logo" />Association for Psychological Science</li>
+    <a href="http://www.psychologicalscience.org">
+    <img src="/image/aps.gif" alt="aps logo" />Association for Psychological Science
+    </a>
+  </li>
 </ul>
