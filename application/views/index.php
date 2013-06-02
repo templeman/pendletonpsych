1972b8bbd6c2a2c663df546918c01257450e83b4
templeman <samuel.templeman@me.com>
2013-06-01 18:09:07 -0700

    Add new office announcement

--- application/views/index.php
+++ application/views/index.php
@@ -1,15 +1,19 @@
-		<a class="special-content" href="your-mental-health/holiday-stress">
-			<div><h3>Stressed Out?</h3>
-			<p>Click here to learn about different <strong>holiday
-				stressors</strong> and the simple steps you can take to manage your
-			own stress this holiday season.</p>
-			</div>
-		</a>
+		<!-- <a class="special&#45;content" href="your&#45;mental&#45;health/holiday&#45;stress"> -->
+		<!-- 	<div><h3>Stressed Out?</h3> -->
+		<!-- 	<p>Click here to learn about different <strong>holiday -->
+		<!-- 		stressors</strong> and the simple steps you can take to manage your -->
+		<!-- 	own stress this holiday season.</p> -->
+		<!-- 	</div> -->
+		<!-- </a> -->
+
+    <div class="special-content">
+      <h3>New Office Opening</h3>
+      <p>Psychological Services of Pendleton, LLC is opening a branch office in Hermiston on June 17, 2013.  The address is 1050 West Elm Avenue, Suite 250.  Dr. Heather Bacon Ph.D., licensed psychologist, is available for appointments in Hermiston on Mondays and Fridays from 8 to 5:30 pm.  Appointments may be made by calling the Hermiston office at 541 667 7899.</p>
+    </div>
 
 		<h2>psychological services of pendleton</h2>
 		<p>Psychological Services of Pendleton, L.L.C. is a full service
-		outpatient treatment center providing assessment and psychotherapy for a 
-broad range of psychological problems. All primary
+		outpatient treatment center providing assessment and psychotherapy for a broad range of psychological problems. All primary
 		clinicians are licensed in Oregon to provide services within their
 		specialty areas. We are located at 135 SE First Street, Pendleton,
 		Oregon, 97801. Explore our website or call 541 278 2222 for more
@@ -22,21 +26,23 @@ broad range of psychological problems. All primary
 		and social relations. We provide the tools, and you set the
 		goals.</p>
 
-		<!--    <h3>Announcements</h3>
-		<h3><a href="PSP DOCS/parenting_ad.pdf">The Art and Science of
-				Parenting</a><img src="image/coming_up_01.gif" width="69"
-			height="10" hspace="15" /></h3>
-		<p>Dr. Terry Templeman Ph.D. psychologist will present a two part
-		program on The Art and Science of Parenting on Tuesday October 27th
-		and Tuesday November 3rd from 7 to 8:30 pm at Sunridge Middle School.
-		The presentation is free and designed for parents of children of all
-		ages. Topics will include Why Parents Matter, Understanding Your
-		Child, Behavior Management Tools that Really Work, and Survival Skills
-		for Parents. The program will include refreshments and interactive
-		discussion with parents. For more information contact Psychological
-		Services of Pendleton LLC at 278 2222.</p>-->
 
-		<!--<h1 class="special">May Is National Mental Health Month</h3>
-		<h2 class="special">Check it out at <a
-				href="http://www.liveyourlifewell.org">www.liveyourlifewell.org</a></h3>
-	</div>-->
+
+	<!-- 	<h3>Announcements</h3> -->
+	<!-- 	<h3><a href="PSP DOCS/parenting_ad.pdf">The Art and Science of -->
+	<!-- 			Parenting</a><img src="image/coming_up_01.gif" width="69" -->
+	<!-- 		height="10" hspace="15" /></h3> -->
+	<!-- 	<p>Dr. Terry Templeman Ph.D. psychologist will present a two part -->
+	<!-- 	program on The Art and Science of Parenting on Tuesday October 27th -->
+	<!-- 	and Tuesday November 3rd from 7 to 8:30 pm at Sunridge Middle School. -->
+	<!-- 	The presentation is free and designed for parents of children of all -->
+	<!-- 	ages. Topics will include Why Parents Matter, Understanding Your -->
+	<!-- 	Child, Behavior Management Tools that Really Work, and Survival Skills -->
+	<!-- 	for Parents. The program will include refreshments and interactive -->
+	<!-- 	discussion with parents. For more information contact Psychological -->
+	<!-- 	Services of Pendleton LLC at 278 2222.</p> -->
+
+	<!-- 	<h1 class="special">May Is National Mental Health Month</h3> -->
+	<!-- 	<h2 class="special">Check it out at <a -->
+	<!-- 			href="http://www.liveyourlifewell.org">www.liveyourlifewell.org</a></h3> -->
+	<!-- </div> -->
