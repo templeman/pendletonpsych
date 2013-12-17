931d2e76984af544c4387ab5ecdeab5e61f60dd7
templeman <samuel.templeman@me.com>
2013-12-17 00:16:27 -0800

    Swap seasonal images and homepage call-outs

--- application/views/index.php
+++ application/views/index.php
@@ -1,17 +1,10 @@
-		<!-- <a class="special&#45;content" href="your&#45;mental&#45;health/holiday&#45;stress"> -->
-		<!-- 	<div><h3>Stressed Out?</h3> -->
-		<!-- 	<p>Click here to learn about different <strong>holiday -->
-		<!-- 		stressors</strong> and the simple steps you can take to manage your -->
-		<!-- 	own stress this holiday season.</p> -->
-		<!-- 	</div> -->
-		<!-- </a> -->
-
-    <a class="special-content" href="/staff#natalie">
-      <div>
-        <h3>PSP Welcomes New Psychologist</h3>
-        <p>Click to learn more about Dr. Natalie Kollross, the newest member of the team!</p>
-      </div>
-    </a>
+		<a class="special-content" href="your-mental-health/holiday-stress">
+			<div><h3>Stressed Out?</h3>
+			<p>Click here to learn about different <strong>holiday
+				stressors</strong> and the simple steps you can take to manage your
+			own stress this holiday season.</p>
+			</div>
+		</a>
 
 		<h2>psychological services of pendleton</h2>
 		<p>Psychological Services of Pendleton, L.L.C. is a full service
