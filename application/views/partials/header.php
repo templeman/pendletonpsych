892bcd930540db0fd021f237113b9a10cb1abaf5
templeman <samuel.templeman@me.com>
2014-06-11 23:06:28 -0700

    Switch from RVM to rbenv

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -14,19 +14,18 @@ $this->load->helper('text');
 		echo $this->sprinkle->output();
 		echo $this->sprinkle->output('browser-fixes');
 	?>
-	<script type="text/javascript">
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-19370004-1']);
-		_gaq.push(['_trackPageview']);
+  ga('create', 'UA-19370004-1', 'pendletonpsych.com');
+  ga('send', 'pageview');
 
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-
-	</script>
+</script>
+<!-- End Google Analytics -->
 </head>
 
 <body id="<?php echo $page_id; ?>" <?php if(!empty($body_extras)) { echo $body_extras; } ?>>
