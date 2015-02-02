46f938e8937c431fcfb1d87c04b1bb5361c77972
templeman <samuel.templeman@me.com>
2015-02-01 21:48:55 -0800

    Add new What's New article

--- application/views/education.php
+++ application/views/education.php
@@ -6,7 +6,7 @@ experience in assessment and psychotherapy is available to students
 interested in practicing in a rural setting. Applicants should be
 currently enrolled in a graduate program in clinical psychology.</p><p>Email
 Dr. Templeman at <a
-	href="mailto:opportunities@pendletonpsych.com?subject=Education%20Opportunities%20at%20PSP">opportunities@pendletonpsych.com</a>
+  href="mailto:opportunities@pendletonpsych.com?subject=Education%20Opportunities%20at%20PSP">opportunities@pendletonpsych.com</a>
 for more information.</p>
 
 <h3>Residency in Psychology</h3>
@@ -16,5 +16,5 @@ experience in assessment and psychotherapy is provided by licensed
 psychologists. Applicants must have doctorates in psychology from
 psychology programs approved by the Oregon Board of Psychologist
 Examiners.</p><p>Email Dr. Templeman at <a
-	href="mailto:opportunities@pendletonpsych.com?subject=Education%20Opportunities%20at%20PSP">opportunities@pendletonpsych.com</a>
+  href="mailto:opportunities@pendletonpsych.com?subject=Education%20Opportunities%20at%20PSP">opportunities@pendletonpsych.com</a>
 for information about terms and earnings.</p>
--- application/views/resources.php
+++ application/views/resources.php
@@ -1,31 +1,31 @@
 <h2>other resources</h2>
 <ul class="panels">
-	<li>
+  <li>
     <a href="http://www.oregon.gov/OBPE/">
     <img src="/image/obpe.gif" alt="obpe logo" />Oregon Board of Psychologist Examiners
     </a>
   </li>
-	<li>
+  <li>
     <a href="http://www.oregon.gov/BLSW/">
     <img src="/image/blcsw.jpg" alt="board logo" />Oregon Board of Clinical Social Workers
     </a>
   </li>
-	<li>
+  <li>
     <a href="http://www.osbn.state.or.us">
     <img src="/image/osbn.jpg" alt="osbn logo" />Oregon State Board of Nursing
     </a>
   </li>
-	<li>
+  <li>
     <a href="http://opa.org">
     <img src="/image/opa.gif" alt="opa logo" />Oregon Psychological Association
     </a>
   </li>
-	<li>
+  <li>
     <a href="http://www.apa.org">
     <img src="/image/apa.gif" alt="apa logo" />American Psychological Association
     </a>
   </li>
-	<li>
+  <li>
     <a href="http://www.psychologicalscience.org">
     <img src="/image/aps.gif" alt="aps logo" />Association for Psychological Science
     </a>
