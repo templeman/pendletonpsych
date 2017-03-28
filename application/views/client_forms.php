24c4a3972dda91e006d2b0abd4bf1687e56ad17a
Sam Templeman <samuel.templeman@me.com>
2017-03-28 14:48:40 -0700

    Update client forms

--- application/views/client_forms.php
+++ application/views/client_forms.php
@@ -4,28 +4,28 @@
   <h3><a href="#">Dr. Templeman's Forms</a></h3>
   <div>
     <ul>
-      <li><a class="pdf" href="/doc/gen-info-2013.pdf">General Information</a></li>
-      <li><a class="pdf" href="/doc/gen-info-minor-dependant-2013.pdf">General Information for Minors/Dependants</a></li>
+      <li><a class="pdf" href="/doc/general-info-adult.pdf">General Information for Adults</a></li>
+      <li><a class="pdf" href="/doc/general-info-child.pdf">General Information for Minors/Dependants</a></li>
       <li><a class="pdf" href="/doc/templeman-consent-to-treat.pdf">Consent to Treatment</a></li>
-      <li><a class="pdf" href="/doc/templeman-hipaa.pdf">HIPAA Consent</a></li>
-      <li><a class="pdf" href="/doc/templeman-consent-assessment.pdf">Consent to Assessment</a></li>
+      <li><a class="pdf" href="/doc/templeman-hipaa-consent.pdf">HIPAA Consent</a></li>
+      <li><a class="pdf" href="/doc/templeman-consent-to-evaluation.pdf">Consent to Psychological Evaluation</a></li>
       <li class="linkinfo">Form only to be completed if you are requesting a psychological evaluation or neuropsychological evaluation. Please contact our office by phone for these requests (541-278-2222).</li>
-      <li><a class="pdf" href="/doc/templeman-release-two-way.pdf">Release Two Way</a></li>
+      <li><a class="pdf" href="/doc/templeman-release-of-information.pdf">Release of Information</a></li>
       <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
     </ul>
   </div>
   <h3><a href="#">Dr. Umphred's Forms</a></h3>
   <div>
     <ul>
-      <li><a class="pdf" href="/doc/gen-info-2013.pdf">General Information</a></li>
-      <li><a class="pdf" href="/doc/gen-info-minor-dependant-2013.pdf">General Information for Minors/Dependants</a></li>
+      <li><a class="pdf" href="/doc/general-info-adult.pdf">General Information for Adults</a></li>
+      <li><a class="pdf" href="/doc/general-info-child.pdf">General Information for Minors/Dependants</a></li>
       <li><a class="pdf" href="/doc/patient-history-form.pdf">Patient History</a></li>
       <li><a class="pdf" href="/doc/childhood-history.pdf">Childhood History</a></li>
       <li><a class="pdf" href="/doc/umphred-consent-to-treat.pdf">Consent to Treatment</a></li>
-      <li><a class="pdf" href="/doc/umphred-hipaa.pdf">HIPAA Consent</a></li>
-      <li><a class="pdf" href="/doc/umphred-consent-assessment.pdf">Consent to Assessment</a></li>
+      <li><a class="pdf" href="/doc/umphred-hipaa-consent.pdf">HIPAA Consent</a></li>
+      <li><a class="pdf" href="/doc/umphred-consent-to-evaluation.pdf">Consent to Psychological Evaluation</a></li>
       <li class="linkinfo">Form only to be completed if you are requesting a psychological evaluation or neuropsychological evaluation. Please contact our office by phone for these requests (541-278-2222).</li>
-      <li><a class="pdf" href="/doc/umphred-release-two-way.pdf">Release Two Way</a></li>
+      <li><a class="pdf" href="/doc/umphred-release-of-information.pdf">Release of Information</a></li>
       <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
     </ul>
   </div>
@@ -33,20 +33,20 @@
   <div>
     <h4>English</h4>
     <ul>
-      <li><a class="pdf" href="/doc/gen-info-2013.pdf">General Information</a></li>
-      <li><a class="pdf" href="/doc/gen-info-minor-dependant-2013.pdf">General Information for Minors/Dependants</a></li>
+      <li><a class="pdf" href="/doc/general-info-adult.pdf">General Information for Adults</a></li>
+      <li><a class="pdf" href="/doc/general-info-child.pdf">General Information for Minors/Dependants</a></li>
       <li><a class="pdf" href="/doc/kollross-consent-to-treat-en.pdf">Consent to Treatment</a></li>
       <li><a class="pdf" href="/doc/kollross-consent-to-treat-children-of-divorced-parents-en.pdf">Consent to Treat Children of Divorced Parents</a></li>
-      <li><a class="pdf" href="/doc/kollross-hipaa-en.pdf">HIPAA Consent</a></li>
-      <li><a class="pdf" href="/doc/kollross-consent-assessment-en.pdf">Consent to Assessment</a></li>
+      <li><a class="pdf" href="/doc/kollross-hipaa-consent-en.pdf">HIPAA Consent</a></li>
+      <li><a class="pdf" href="/doc/kollross-consent-to-evaluation-en.pdf">Consent to Psychological Evaluation</a></li>
       <li class="linkinfo">Form only to be completed if you are requesting a psychological evaluation or neuropsychological evaluation. Please contact our office by phone for these requests (541-278-2222).</li>
-      <li><a class="pdf" href="/doc/kollross-release-two-way-en.pdf">Release Two Way</a></li>
+      <li><a class="pdf" href="/doc/kollross-release-two-way-en.pdf">Release of Information: Two Way</a></li>
       <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
-      <li><a class="pdf" href="/doc/kollross-release-one-way-clinician-to-other-en.pdf">Release One Way from Clinician</a></li>
+      <li><a class="pdf" href="/doc/kollross-release-one-way-clinician-to-other-en.pdf">Release of Information: One Way from Clinician</a></li>
       <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
-      <li><a class="pdf" href="/doc/kollross-release-one-way-to-clinician-en.pdf">Release One Way to Clinician</a></li>
+      <li><a class="pdf" href="/doc/kollross-release-one-way-to-clinician-en.pdf">Release of Information: One Way to Clinician</a></li>
       <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
-      <li><a class="pdf" href="/doc/kollross-release-one-way-to-clinician-confidential-en.pdf">Release One Way to Clinician Confidential</a></li>
+      <li><a class="pdf" href="/doc/kollross-release-one-way-to-clinician-confidential-en.pdf">Release of Information: One Way to Clinician Confidential</a></li>
       <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
     </ul>
     <h4>Spanish</h4>
@@ -71,15 +71,21 @@
   <h3><a href="#">Dr. Evans' Forms</a></h3>
   <div>
     <ul>
-      <li><a class="pdf" href="/doc/gen-info-2013.pdf">General Information</a></li>
-      <li><a class="pdf" href="/doc/gen-info-minor-dependant-2013.pdf">General Information for Minors/Dependants</a></li>
+      <li><a class="pdf" href="/doc/general-info-adult.pdf">General Information for Adults</a></li>
+      <li><a class="pdf" href="/doc/general-info-child.pdf">General Information for Minors/Dependants</a></li>
       <li><a class="pdf" href="/doc/patient-history-form.pdf">Patient History</a></li>
       <li><a class="pdf" href="/doc/childhood-history-2.pdf">Childhood History</a></li>
-      <li><a class="pdf" href="/doc/evans-resident-consent-to-treat.pdf">Consent to Treatment</a></li>
-      <li><a class="pdf" href="/doc/evans-resident-hipaa.pdf">HIPAA Consent</a></li>
-      <li><a class="pdf" href="/doc/evans-resident-consent-assessment.pdf">Consent to Assessment</a></li>
+      <li><a class="pdf" href="/doc/evans-consent-to-treat.pdf">Consent to Treatment</a></li>
+      <li><a class="pdf" href="/doc/evans-hipaa-consent.pdf">HIPAA Consent</a></li>
+      <li><a class="pdf" href="/doc/evans-consent-to-evaluation.pdf">Consent to Psychological Evaluation</a></li>
       <li class="linkinfo">Form only to be completed if you are requesting a psychological evaluation or neuropsychological evaluation. Please contact our office by phone for these requests (541-278-2222).</li>
-      <li><a class="pdf" href="/doc/evans-resident-release-two-way.pdf">Release Two Way</a></li>
+      <li><a class="pdf" href="/doc/evans-release-two-way.pdf">Release of Information: Two Way</a></li>
+      <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
+      <li><a class="pdf" href="/doc/evans-release-one-way-clinician-to-other.pdf">Release of Information: One Way from Clinician</a></li>
+      <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
+      <li><a class="pdf" href="/doc/evans-release-one-way-to-clinician.pdf">Release of Information: One Way to Clinician</a></li>
+      <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
+      <li><a class="pdf" href="/doc/evans-release-one-way-to-clinician-confidential.pdf">Release of Information: One Way to Clinician Confidential</a></li>
       <li class="linkinfo">This is a release of information form. This form is only to be filled out if you would like this provider to obtain or coordinate records with another person or provider.</li>
     </ul>
   </div>
