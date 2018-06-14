c1db8f7bf33517dfe136e4a35b84d748ecdb5826
Sam Templeman <samuel.templeman@me.com>
2018-06-14 08:56:26 -0700

    Add What's New article

--- application/views/whats_new.php
+++ application/views/whats_new.php
@@ -1,9 +1,5 @@
 <h2>What's new in Psychology?</h2>
-<p class="sm"><em>Psychological Services of Pendleton, LLC is committed
-	to public education about research and treatment of psychological
-	problems.  Watch this column for announcements about public
-	presentations and workshops on selected topics, and also for web other
-	resources and articles on selected topics.</em></p>
+<p class="sm"><em>Psychological Services of Pendleton, LLC is committed to public education about research and treatment of psychological problems. Watch this column for announcements about public presentations and workshops on selected topics, and also for other web resources and articles on selected topics.</em></p>
 
 
 <h3><?php echo $title; ?></h3>
